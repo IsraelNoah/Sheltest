ffc666d6c5c29c6d636de00063f45854b5f55026
IsraelNoah <i.noah@shellboxes.com>
2021-07-06 13:09:02 +0200

    update hello programm java

--- Hello.java
+++ Hello.java
@@ -1,9 +1,11 @@
-public class HelloWorld {
+public class Hello {
 
        public static void main(String[] args) {
             // Prints "Hello, World" to the terminal windows.
-	    System.out.println("Hello, World");
+	    for(int i=1;i<=10;i++) {
+	       System.out.println("Hello World .... !!"+i);
 
+            }
        }
 
 }
